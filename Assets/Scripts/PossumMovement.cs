ea860027705e81e9f601d09d5198fdbbf79d24e5
Annie Thorburn <annie@pop-os.localdomain>
2022-06-11 10:01:01 -0400

    fixed typo in movement script

--- Assets/Scripts/PossumMovement.cs
+++ Assets/Scripts/PossumMovement.cs
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-version https://git-lfs.github.com/spec/v1
-oid sha256:0777902306a6ce70903fc936c80d15a0765b8e165188f64261c9d1d8fac33c84
-size 316
-=======
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
